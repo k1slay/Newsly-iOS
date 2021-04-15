3e42cb39548ac689454810638558e1d9a94a5e3b
Kislay <kislay.kishore@inmobi.com>
2021-04-15 21:57:29 +0530

    Initial Commit

---
+++ Newsly/NewslyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewslyApp.swift
+//  Newsly
+//
+//  Created by Kislay Kishore on 15/04/21.
+//
+
+import SwiftUI
+
+@main
+struct NewslyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
